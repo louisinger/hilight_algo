b9dd0ec38b80664af5abe00dda7473ed733c0ab6
louisinger <41042567+louisinger@users.noreply.github.com>
2019-02-01 11:21:58 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,11 @@
 import json
+import hilight_algo_v2/basic_segmentation_twitter_v3
 
 def lambda_handler(event, context):
-    # Return le JSON ici !!
+   
     website_name = event['website']
-    return "TON PUTAIN DE JSON ICI"
+    return if (name == "Twitter"):
+		return basic_segmentation_twitter_v3.find_sentences()
+	else:
+		return "null"
+    
