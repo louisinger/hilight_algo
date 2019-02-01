63f21df31ea733989f30cd56addac7b752e30011
RicodeL <louis.singer.git@gmail.com>
2019-02-01 14:11:09 +0100

    modify the file path parameters

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ import basic_segmentation_twitter_v3
 def lambda_handler(event, context):
     website_name = "Twitter"
     if website_name == "Twitter": 
-        return  basic_segmentation_twitter_v3.find_sentences('./hilight_aglo_v2/twitter.txt', 'english')
+        return  basic_segmentation_twitter_v3.find_sentences(r'./hilight_aglo_v2/twitter.txt', 'english')
     
 	
 
