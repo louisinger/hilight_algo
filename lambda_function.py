24673dc428f6857586335f53742f54e855441b6a
louisinger <41042567+louisinger@users.noreply.github.com>
2019-02-01 11:12:17 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import json
 
 def lambda_handler(event, context):
-    return "Le double de votre nombre est:  " + event['number']*2
+    # Return le JSON ici !!
+    return "TON PUTAIN DE JSON ICI"
