5e3594101a91b1ff1a3801bb8651b94a5d65ed41
RicodeL <louis.singer.git@gmail.com>
2019-02-01 13:00:31 +0100

    ultimate test

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,13 @@
 import json
 import sys
-sys.path.append('./hilight_algo_v2/')
-from hilight_algo_v2.basic_segmentation_twitter_v3 import *
+sys.path.append(r"./hilight_aglo_v2")
+
+import basic_segmentation_twitter_v3
 
 def lambda_handler(event, context):
-   
     website_name = "Twitter"
-    if (name == "Twitter"):
-	return basic_segmentation_twitter_v3.find_sentences()
+    if website_name == "Twitter": 
+        return  basic_segmentation_twitter_v3.find_sentences()
 	
 
     
