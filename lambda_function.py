72578e86077936d10b3c8041ad0cfbc89df2ba78
vi <victor.colleter@efrei.net>
2019-02-01 15:32:40 +0100

    new

--- lambda_function.py
+++ lambda_function.py
@@ -3,13 +3,9 @@ import sys
 sys.path.append(r"./hilight_aglo_v2")
 sys.path.append(r"./package")
 
-import basic_segmentation_twitter_v3
+import prepocessing.py
 
 def lambda_handler(event, context):
     website_name = "Twitter"
-    if website_name == "Twitter": 
-        return  basic_segmentation_twitter_v3.find_sentences('twitter.txt', 'english')
-    
-	
-
-    
+    if website_name == "Twitter":
+        return  preprocessing.find_sentences('twitter.txt', 'english')
