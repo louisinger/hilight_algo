807998ca89794b7fde38c973de71549fddb16519
CALIARI Axel <axel.caliari@efrei.net>
2019-04-08 16:49:52 +0200

    Comparison between cgus

--- visualization/basic_plotly_v2.py
+++ visualization/basic_plotly_v2.py
@@ -4,6 +4,8 @@ import plotly.graph_objs as go
 import plotly.io as pio
 import os
 import json
+import urllib
+import re
 
 py.sign_in('axelc', '6qQk1TZYuAOpw0g7smjc')
 
@@ -13,7 +15,7 @@ categories = ["Personal data", "data controller", "Log data", "account data", "P
 #construit le dictionnaire des mots et leur occurence(grouped words)
 def build_dic_full():
     #on récupère les données qu'on a auparavant analysées
-    f = open('useful_sentence_full.json')
+    f = open('./old/useful_sentence_full.json')
     cgu = json.load(f)
     
     dic = {}
@@ -30,7 +32,7 @@ def build_dic_full():
 #construit le dictionnaire des mots et leur occurence(basic words)
 def build_dic_basic():
     #on récupère les données qu'on a auparavant analysées
-    f = open('useful_sentence_syn.json')
+    f = open('./old/useful_sentence_syn.json')
     cgu = json.load(f)
     
     dic = {}
@@ -153,9 +155,6 @@ def compare_grade_percentage():
     
 
 def compare_from_api(website):
-    import urllib
-    import re
-
     url="https://wh5ya21546.execute-api.eu-west-3.amazonaws.com/dev/hilight/" + website
     json_url = urllib.request.urlopen(url)
     data = json.loads(json_url.read())
@@ -165,7 +164,7 @@ def compare_from_api(website):
 
     data_evaluation = {}
     for i in body:
-        if(i['gradeCriteria'] != -6):
+        if i['gradeCriteria'] != -6:
             data_evaluation[i['nameCriteria']] = i['gradeCriteria']
     
     #pprint(data_evaluation)
@@ -186,11 +185,100 @@ def compare_from_api(website):
     return html
 
 
+def compare_from_api_subcriteria(website):
+    url="https://wh5ya21546.execute-api.eu-west-3.amazonaws.com/dev/hilight/" + website
+    json_url = urllib.request.urlopen(url)
+    data = json.loads(json_url.read())
+    body = json.loads(data['body'])
+    body = re.sub(' +', ' ', body)
+    body = json.loads(body)
+
+    data_evaluation = {}
+    for i in body:
+        if i['gradeCriteria'] != -6:
+            data_evaluation[i['nameCriteria']] = i['subCriterias']
+
+    print(data_evaluation)
+
+def compare_between_cgu(website_list):
+    grade_negatives = {}
+
+    for website in website_list:
+        url="https://wh5ya21546.execute-api.eu-west-3.amazonaws.com/dev/hilight/" + website
+        json_url = urllib.request.urlopen(url)
+        data = json.loads(json_url.read())
+        body = json.loads(data['body'])
+        body = re.sub(' +', ' ', body)
+        body = json.loads(body)
+
+        grades = 0
+        numbers_of_negatives_grades = 0
+        counter = 0
+
+        for i in body:
+            if i['gradeCriteria'] != -6:
+                grades += i['gradeCriteria']
+                counter += 1
+
+                if i['gradeCriteria'] < 0:
+                    numbers_of_negatives_grades += 1
+
+        average = grades/counter
+        grade_negatives[website] = {}
+        grade_negatives[website]['average'] = average
+        grade_negatives[website]['numbers_of_negative_grades'] = numbers_of_negatives_grades
+        
+    data_evaluation = []
+    for website in grade_negatives:
+        trace = go.Scatter(
+            x=[grade_negatives[website]['numbers_of_negative_grades']],
+            y=[grade_negatives[website]['average']],
+            mode='markers',
+            name=website,
+            text=website,
+            marker=dict(
+                size=[40],
+            )
+        )
+        data_evaluation.append(trace)
+
+    layout = go.Layout(
+        title='Comparison between companies',
+        xaxis=dict(
+            title='Number of negative grades',
+            zeroline=True,
+            showline=True,
+            range=[0, 10],
+            zerolinewidth=1,
+            ticklen=5,
+            gridwidth=2,
+        ),
+        yaxis=dict(
+            title='Average grade',
+            zeroline=True,
+            showline=True,
+            range=[-1, 1],
+            zerolinewidth=1,
+            ticklen=5,
+            gridwidth=2,
+        ),
+        paper_bgcolor='rgb(243, 243, 243)',
+        plot_bgcolor='rgb(243, 243, 243)',
+    )
+
+    fig = go.Figure(data=data_evaluation, layout=layout)
+    graph_url = py.plot(fig, filename='bubble-compare')
+
+    html = "<div><a href=" + graph_url + "target='_blank' title='comparison-between-cgus' style='display: block; text-align: center;'><img src=" + graph_url + ".png alt='comparison-from-api' style='max-width: 100%;width: 600px;'  width='600'/></a><script data-plotly=" + graph_url.replace('https://plot.ly/~', '') + " src='https://plot.ly/embed.js' async></script></div>"
+
+    return html
 
 #graph_bar()
 #graph_pie()
 #compare_from_api(website)
+#compare_from_api_subcriteria('twitter')
+print(compare_between_cgu(['twitter', 'facebook', 'amazon']))
 
 
-
- 
+# carte des drogues - bubble chart
+# sous critères - pie
